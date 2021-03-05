4a95a52ca0da2d6afa60d1f35752e9ed76e16038
Tsobirev Max <maks61ts@gmail.com>
2021-03-05 11:12:42 +0200

    Добавьте файлы проекта.

---
+++ zmejka/Parametrs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace zmejka
+{
+    class Parametrs
+    {
+
+        private string resourcesFolder;
+
+        public Parametrs()
+        {
+            var ind = Directory.GetCurrentDirectory().ToString()
+                .IndexOf("bin", StringComparison.Ordinal);
+
+            string binFolder =
+                Directory.GetCurrentDirectory().ToString().Substring(0, ind)
+                    .ToString();
+
+            resourcesFolder = binFolder + "Resources\\";
+        }
+
+        public string GetResourceFolder()
+        {
+            return resourcesFolder;
+        }
+
+
+    }
+}
---
+++ zmejka/Start.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zmejka
+{
+    public class Start
+    {
+        public ConsoleKeyInfo key;
+        public Start()
+        {
+        }
+        int valik = 0;
+        public int choice()
+        {
+
+            Console.SetCursorPosition(0, 5);
+            Console.WriteLine("------------------");
+            Console.WriteLine(" Start game - S\n Stop game - Q");
+            Console.WriteLine("------------------");
+            key = Console.ReadKey(true);
+
+            if (key.Key == ConsoleKey.S)
+            {
+                Console.SetCursorPosition(0, 5);
+                Console.WriteLine("------------------");
+                Console.WriteLine(" Large map - L\n Small map - S");
+                Console.WriteLine("------------------");
+                key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.L)
+                {
+                    valik = 151;
+                }
+                else
+                {
+                    valik = 101;
+                }
+            }
+            else if (key.Key == ConsoleKey.Q)
+            {
+                valik = 2;
+            }
+            return valik;
+        }
+
+        public void Game_stop()
+        {
+            Console.Clear();
+            Console.WriteLine("--------------");
+            Console.WriteLine("              ");
+            Console.WriteLine("     ()_()    ");
+            Console.WriteLine("    ( o_- )   ");
+            Console.WriteLine("     ()o()    ");
+            Console.WriteLine("     ()_()    ");
+            Console.Beep();
+
+            Environment.Exit(1);
+
+
+        }
+    }
+}
